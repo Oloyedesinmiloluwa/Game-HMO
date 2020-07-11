fdc25d212dd9bc90a944b8a039ce593bcc579ec8
Sinmiloluwa Oloyede <sinmiloluwasunday@yahoo.com>
2020-07-11 16:55:43 +0100

    Update code

--- app/Console/Commands/GenerateData.php
+++ app/Console/Commands/GenerateData.php
@@ -43,47 +43,26 @@ class GenerateData extends Command
      */
     public function handle()
     {
-        dump($this->generatePlayers());
-        dump($this->generateGames());
-        dump($this->generateGamePlays());
+        dump($this->generatePlayers(), 'done generating players');
+        dump($this->generateGames(), 'done generating games');
+        dump($this->generateGamePlays(), 'done generating game plays');
         return 0;
-    }
-
-    public function generateData()
-    {
-
-        $game = Games::create([
-            'name' => 'Mortal Kombat',
-            'version' => '2010'
-        ]);
-        return $game;
     }
 
     public function generatePlayers()
     {
-        //10000
         $players = [];
-        for ($i=0; $i < 10; $i++) {
+        for ($i=0; $i < 10000; $i++) {
             $data = [
                 'name' => $this->faker->name,
                 'nickname' => $this->faker->firstName,
                 'email' => $this->faker->email,
                 'password' => Hash::make($this->faker->password),
-                'last_login' => now()// new \DateTime()
+                'last_login' => now()
             ];
             array_push($players, $data);
         }
         return Players::insert($players);
-
-        // Players::create(
-        //     [
-        //         'name' => $this->faker->name,
-        //         'nickname' => $this->faker->firstName,
-        //         'email' => $this->faker->email,
-        //         'password' => Hash::make($this->faker->password),
-        //         'last_login' => now()// new \DateTime()
-        //     ]
-        //     );
     }
 
     public function generateGames()
@@ -135,38 +114,16 @@ class GenerateData extends Command
     public function generateGamePlays()
     {
 
-        //3,835 days of gaming can start from 2010
-        // at least 1500 players must have a game play per day and one of them can host or not
-        // the system should handle who started a game and the invitees but each player has a separate record
-            // with only max of 4 players
-            // and they must have the same game version
-        // add game versions as the years in the loop, may not have to
-        // instead add games and their versions separately
-
-        $players = Players::all()->skip(0)->take(3)->toArray();
-        // dump($players, 'players');
-        // die();
+        $players = Players::all()->skip(0)->take(1500)->toArray();
 
         $games = Games::all();
-        // dump($games[1], 'games format');
-        // die();
         $startDate = new \DateTime('2010-01-01T05:42:58.000000Z');
-        // dump($startDate, 'sgtart date');
-        // die();
-        // try {
-        //     //code...
-        // } catch (\Throwable $th) {
-        //     //throw $th;
-        // }
-        for ($i=1; $i < 12; $i++) { //3836
+
+        for ($i=1; $i < 3836; $i++) {
 
             // represent 1500 players each day
             foreach ($players as $key => $player) {
 
-                // //games may not have value
-                // foreach ($games as $key => $game) {
-
-                // }
                 $currentDate = $startDate->getTimestamp() + ($i * 24 * 60 * 60);
                 $isNotValid = true;
                 $countSafe = 0;
@@ -174,30 +131,21 @@ class GenerateData extends Command
                 while ($isNotValid || $countSafe === count($games)) {
                     $countSafe += 1;
                     $gameIndex = rand(0, count($games) - 1);
-                    // dump($gameIndex, 'index');
                     $gamePlayed = $games[$gameIndex];
-                    // dump($gamePlayed, 'played');
-                    // die();
+
                     if($currentDate > $gamePlayed->created_at->getTimestamp()) {
                         $isNotValid = false;
                     }
                 }
 
-                // dump(new \DateTime('@' . $currentDate), 'date');die();
 
-                // dump($gamePlayed, 'played');
-                // die();
-                // dump($player, 'player');
-                // die();
                 $gamemi = Gameplays::create([
-                    'gameId' => $gamePlayed->id, //make _
+                    'gameId' => $gamePlayed->id,
                     'playerId' => $player['id'],
                     'isHost' => true,
                     'created_at' =>  new \DateTime('@' . $currentDate),
                     'updated_at' => new \DateTime('@' . $currentDate),
                 ]);
-                dump($gamemi, 'gameplayed');
-                // use year added
             }
         }
     }
